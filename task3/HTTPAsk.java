5f973e944caedad6a3596521df76ec037cd5b74a
NicoleWij <nwijkman@kth.se>
2022-02-23 00:05:53 +0100

    update

--- task3/HTTPAsk.java
+++ task3/HTTPAsk.java
@@ -4,70 +4,78 @@ import java.nio.charset.StandardCharsets;
 import tcpclient.TCPClient;
 
 public class HTTPAsk {
-    public static void main( String[] args) {
+    public static void main(String[] args) {
+        String hostname = null;
+        byte[] inp = new byte[0];
+        int port = 0;
+        Boolean shutdown = false;
+        Integer limit = null;
+        Integer timeout = null;
+
         try {
             ServerSocket http = new ServerSocket(Integer.parseInt(args[0]));
-            String hostname = null;
-            int port = -1;
-            byte[] userInput = {'h', 'e', 'l', 'l', 'o'};
-            Integer limit = null;
-            Integer timeout = null;
-            Boolean shutdown = null;
 
-            while(true){
+            while (true) {
                 Socket socket = http.accept();
-                OutputStream output = socket.getOutputStream();
-                output.write(userInput);
-
                 byte[] buffer = new byte[1024];
+
                 socket.getInputStream().read(buffer);
+                OutputStream output = socket.getOutputStream();
+                
+                StringBuilder stringBuild = new StringBuilder();
 
                 String url = new String(buffer, StandardCharsets.UTF_8);
                 String[] urlArray = new String[url.length()];
 
-                System.out.println("URL = " + url);
-
                 url = url.replace("?", " ");
                 url = url.replace("=", " ");
                 url = url.replace("&", " ");
+                url = url.replace("\r\n", " ");
                 urlArray = url.split(" ");
 
-                System.out.println("URL ARRAY = " + urlArray[1]);
-
-                if(!(url.contains("ask"))){
-                    System.out.println("Error 404 not found");
-                    break;
+                if (!(url.contains("ask"))) {
+                    stringBuild.append("HTTP/1.1 404 Not Found\r\n");
+                    output.write(stringBuild.toString().getBytes());
+                    socket.close();
+                    continue;
                 }
 
-                if(!((url.contains("hostname") || url.contains("port")))){
-                    System.out.println("400 Bad request");
-                    break;
+                if (!(url.contains("hostname")) || !(url.contains("port"))) {
+                    stringBuild.append("HTTP/1.1 400 Bad Request\r\n");
+                    output.write(stringBuild.toString().getBytes());
+                    socket.close();
+                    continue;
                 }
 
-                for(int i = 2; i < urlArray.length; i++){
+                for (int i = 2; i < urlArray.length; i++) {
                     switch (urlArray[i]) {
                         case "hostname":
                             hostname = urlArray[++i];
 
                             break;
-                    
+
                         case "port":
                             port = Integer.parseInt(urlArray[++i]);
 
                             break;
-                        
+
                         case "limit":
                             limit = Integer.valueOf(urlArray[++i]);
 
                             break;
-                    
+
                         case "timeout":
                             timeout = Integer.valueOf(urlArray[++i]);
 
                             break;
-                    
+
                         case "shutdown":
                             shutdown = Boolean.parseBoolean(urlArray[++i]);
+
+                            break;
+
+                        case "string":
+                            inp = urlArray[++i].getBytes();
 
                             break;
 
@@ -76,14 +84,19 @@ public class HTTPAsk {
                     }
                 }
 
-                // TCPClient tcp = new tcpclient.TCPClient(shutdown, timeout, limit);
-                // tcp.askServer(hostname, port, userInput);
+                stringBuild.append("HTTP/1.1 200 OK\r\n\r\n");
+                TCPClient tcp = new TCPClient(shutdown, timeout, limit);
+                stringBuild.append(new String(tcp.askServer(hostname, port, inp)));
+                stringBuild.append("\r\n");
 
-                // output.flush();
+                System.out.println(stringBuild);
+
+                output.write(stringBuild.toString().getBytes());
+                socket.close();
             }
 
         } catch (Exception e) {
-            System.out.println("ERROR");
+            System.out.println("Error is: " + e);
         }
     }
 }
